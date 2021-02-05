19a47e317fcac494538103179232a5af01dea4a8
Ehsan Haghighat <haghighat.ir@gmail.com>
2021-02-05 00:38:36 -0800

    V0.6.0.0 - Google Colab bug fix + Adaptive Weights

--- sciann/__init__.py
+++ sciann/__init__.py
@@ -33,7 +33,7 @@ __copyright__ = "Copyright 2019, Physics-Informed Deep Learning"
 __credits__ = []
 __url__ = "http://github.com/sciann/sciann]"
 __license__ = "MIT"
-__version__ = "0.5.5.0"
+__version__ = "0.6.0.0"
 __cite__ = \
     '@misc{haghighat2019sciann, \n' +\
     '    title={SciANN: A Keras/Tensorflow wrapper for scientific computations and physics-informed deep learning using artificial neural networks}, \n' +\
